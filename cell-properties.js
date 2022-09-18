d8d17e03427d6d0da360e9450ba3c7844db43ae9
Mohd Danish Saifi <saifidanish549@gmail.com>
2022-09-18 12:16:46 +0530

    cycle detection and path trace done finally

--- cell-properties.js
+++ cell-properties.js
@@ -1,24 +1,3 @@
-for (let i = 0; i < rows; i++) {
-    let sheetRow = [];
-    for (let j = 0; j < cols; j++) {
-        let cellProp = {
-            bold: false,
-            italic: false,
-            underline: false,
-            alignment: "left",
-            fontFamily: "monospace",
-            fontSize: "14",
-            fontColor: "#000000",
-            BGcolor: "#000000",
-            value: "",
-            formula:"",
-            children: [],
-        }
-        sheetRow.push(cellProp);
-    }
-    sheetDB.push(sheetRow);
-}
-
 // let collectedSheetDB = [];  //Contains all SheetDB
 let sheetDB = [];
 
